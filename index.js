f61e38cf701013eb0fbc7b3a4800951fc5c0dce8
OguzhanAtasever <oguzhanatasever@gmail.com>
2019-05-27 02:29:37 +0300

    transaction pool the blockchain and cryptocurrency backend

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
-const pubsub = new PubSub({ blockchain });
+const pubsub = new PubSub({ blockchain ,transactionPool });
 
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRES = `http://localhost:${DEFAULT_PORT}`;
@@ -52,6 +52,7 @@ app.post('/api/transact',(req, res) => {
 
     transactionPool.setTransaction(transaction);
 
+    pubsub.broadcastTransaction(transaction);
 
     res.json({type:'success',transaction })
 });
@@ -60,13 +61,22 @@ app.get('/api/transaction-pool-map',(req, res) => {
     res.json(transactionPool.transactionMap);
 });
 
-const syncChain = ()=>{
+const syncWithRootState = ()=>{
     request({url: `${ROOT_NODE_ADDRES}/api/blocks`},(error,response, body)=>{
         if(!error && response.statusCode ===200){
             const rootChain = JSON.parse(body);
 
             console.log('replace chain on a sync with',rootChain);
             blockchain.replaceChain(rootChain);
+        }
+    });
+
+    request({ url: `${ROOT_NODE_ADDRES}/api/transaction-pool-map`},(error, response, body) => {
+        if(!error && response.statusCode === 200){
+            const rootTransactionPoolMap = JSON.parse(body);
+
+            console.log('replace transaction pool map on a sync with', rootTransactionPoolMap);
+            transactionPool.setMap(rootTransactionPoolMap);
         }
     });
 };
@@ -82,7 +92,7 @@ app.listen(PORT,()=> {
     console.log(`listening at localhost:${PORT}`);
 
     if(PORT !== DEFAULT_PORT){
-        syncChain();
+        syncWithRootState();
     }
     
 });
